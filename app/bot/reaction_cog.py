3d5d118e669fe468478919c682062420418c673e
Diego Dias <diego2007jf@gmail.com>
2024-10-17 23:34:16 -0300

    added reaction cog

---
+++ app/bot/reaction_cog.py
@@ -0,0 +1,21 @@
+from datetime import timedelta
+from typing import Union
+
+import discord
+from discord.ext import commands
+from app.services.logging_service import logger
+
+
+class ReactionCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction: discord.Reaction, user: Union[discord.User, discord.Member]):
+        if reaction.message.author == self.bot.user:
+            logger.info(f'Detected reaction "{reaction}"')
+            await reaction.remove(user)
+            logger.info(f'Removed reaction "{reaction}"')
+            await user.timeout_for(duration=timedelta(seconds=300), reason='Não use Mudae.')
+            return logger.info(f'User "{user.display_name}" has been muted.')
+
