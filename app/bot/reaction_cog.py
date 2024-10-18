e7b67c0cb54b5a84f6375a8ddcd7e1ed5a498bff
Diego Dias <diego2007jf@gmail.com>
2024-10-18 02:00:33 -0300

    fixed possible bugs

--- app/bot/reaction_cog.py
+++ app/bot/reaction_cog.py
@@ -12,18 +12,20 @@ class ReactionCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction: discord.Reaction, user: Union[discord.User, discord.Member]):
-        if reaction.message.author == self.bot.user:
-            try:
-                logger.info(f'Detected reaction "{reaction}" from user "{user.display_name}".')
-                await reaction.remove(user)
-                logger.info(f'Removed reaction "{reaction}" from user "{user.display_name}".')
-                await user.timeout_for(duration=timedelta(seconds=300), reason='Não use Mudae.')
-                return logger.info(f'User "{user.display_name}" has been muted.')
-            except (commands.MissingPermissions, discord.errors.Forbidden) as err:
-                logger.error(f'Failed to fully execute script due to "{err}".')
-                embed: discord.Embed = reaction.message.embeds[0]
-                embed.colour = discord.Colour.dark_red()
-                await reaction.message.edit(embed=embed)
-                return await reaction.message.channel.send(f'💖 **{user.name}** and **{embed.title}** are now married! 💖')
+        if reaction.message.author == self.bot.user and reaction.message.embeds:
+            if reaction.message.embeds[0].colour != discord.Colour.dark_red():
+                try:
+                    logger.info(f'Detected reaction "{reaction}" from user "{user.display_name}".')
+                    await reaction.remove(user)
+                    logger.info(f'Removed reaction "{reaction}" from user "{user.display_name}".')
+                    await user.timeout_for(duration=timedelta(seconds=300), reason='Não use Mudae.')
+                    return logger.info(f'User "{user.display_name}" has been muted.')
+                except (commands.MissingPermissions, discord.errors.Forbidden) as err:
+                    logger.error(f'Failed to fully execute script due to "{err}".')
+                    embed: discord.Embed = reaction.message.embeds[0]
+                    embed.colour = discord.Colour.dark_red()
+                    await reaction.message.edit(embed=embed)
+                    return await reaction.message.channel.send(f'💖 **{user.name}** and '
+                                                               f'**{embed.title}** are now married! 💖')
 
 
