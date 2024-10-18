393990b29fe76da8cf5f1d442991fb61817ede97
Diego Dias <diego2007jf@gmail.com>
2024-10-18 01:27:00 -0300

    added base commands

--- app/bot/commands_cog.py
+++ app/bot/commands_cog.py
@@ -9,5 +9,25 @@ class CommandsCog(commands.Cog):
 
     @commands.command()
     async def wa(self, ctx: commands.Context):
+        """Waifu Anime"""
         logger.info(f'User "{ctx.author.display_name}" used command "wa".')
         return await ctx.send('hello')
+
+    @commands.command()
+    async def ha(self, ctx: commands.Context):
+        """Husbando Anime"""
+        logger.info(f'User "{ctx.author.display_name}" used command "ha".')
+        return await ctx.send('hello')
+
+    @commands.command()
+    async def wg(self, ctx: commands.Context):
+        """Waifu Game"""
+        logger.info(f'User "{ctx.author.display_name}" used command "wg".')
+        return await ctx.send('hello')
+
+    @commands.command()
+    async def hg(self, ctx: commands.Context):
+        """Husbando Game"""
+        logger.info(f'User "{ctx.author.display_name}" used command "hg".')
+        return await ctx.send('hello')
+
