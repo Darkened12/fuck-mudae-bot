8856f4e9a0b883b6219bd89f4220f795edfcd4ca
Diego Dias <diego2007jf@gmail.com>
2024-10-19 03:36:59 -0300

    added husbando datasets

---
+++ app/models/husbando_anime_dataset.py
@@ -0,0 +1,13 @@
+from .base_model import MudaeModel
+from typing import List
+
+
+anime_husbandos: List[MudaeModel] = [
+    MudaeModel('Satoru Gojo', 'Jujutsu Kaisen', 'https://imgur.com/BSBVM3I'),
+    MudaeModel('Monkey D. Luffy', 'One Piece', 'https://imgur.com/m11lKX4'),
+    MudaeModel('Kirito', 'Sword Art Online', 'https://imgur.com/S7STxra'),
+    MudaeModel('Lelouch Lamperouge', 'Code Geass: Hangyaku no Lelouch', 'https://mudae.net/uploads/6526466/qvf_RQ9~RPkuWAX.png'),
+    MudaeModel('Cid Kagenou', 'Kage no Jitsuryokusha ni Naritakute!', 'https://cdn.imgchest.com/files/84jdc8ldow4.gif'),
+    MudaeModel('Sanji', 'One Piece', 'https://cdn.imgchest.com/files/my8xcgdjq24.gif'),
+    MudaeModel('Naruto Uzumaki', 'Naruto', 'https://imgur.com/lZ5F073'),
+]
---
+++ app/models/husbando_game_dataset.py
@@ -0,0 +1,13 @@
+from .base_model import MudaeModel
+from typing import List
+
+
+game_husbandos: List[MudaeModel] = [
+    MudaeModel('Shadow the Hedgehog', 'Sonic Adventures Series', 'https://imgur.com/H84kMCZ'),
+    MudaeModel('Alhaitham', 'Genshin Impact', 'https://imgur.com/OEIgFzd'),
+    MudaeModel('Gallagher', 'Honkai: Star Rail', 'https://mudae.net/uploads/1737574/oR8P40O~40LF9cb.png'),
+    MudaeModel('Neuvillette', 'Genshin Impact', 'https://cdn.imgchest.com/files/w7pjc8bmo37.gif'),
+    MudaeModel('Dante', 'Devil May Cry Series', 'https://cdn.imgchest.com/files/pyq9c66k6d4.gif'),
+    MudaeModel('Hazama', 'BlazBlue: Calamity Trigger', 'https://imgur.com/XI1TjUj'),
+    MudaeModel('Leon S. Kennedy', 'Resident Evil 4', 'https://mudae.net/uploads/2739621/eV3a2Ld~Y3S6kBq.png'),
+]
